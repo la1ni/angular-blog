1e2f7a1b94892bfecc83dd7a8bcefaf335373996
la1ni <marilainarasilva@hotmail.com>
2024-09-18 10:14:55 -0300

    :sparkles: feat: criação de componentes da home

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+import { elementAt } from 'rxjs';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrl: './big-card.component.css'
+})
+
+export class BigCardComponent {
+
+  @Input()
+  pictureCover:string = ""
+
+  @Input()
+  title:string = ""
+
+  @Input()
+  description:string = ""
+
+  text: string = ""
+
+  decorate(){
+    this.text = "text-decorator-class"
+  }
+
+  decorateOff(){
+    this.text = ""
+  }
+
+}
+
+
+
---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent {
+
+  text: string = ""
+
+  @Input()
+  title:string = ""
+
+  @Input()
+  author:string = ""
+
+  @Input()
+  authorImg:string = ""
+
+  @Input()
+  pictureSrc:string = ""
+
+  decorate(event: MouseEvent): void{
+    const elementoAlvo = event.target as HTMLElement;
+    elementoAlvo.style.textDecoration = 'underline';
+  }
+
+  decorateOff(event: MouseEvent): void{
+    const elementoAlvo = event.target as HTMLElement;
+    elementoAlvo.style.textDecoration = 'none';
+
+  }
+
+
+}
